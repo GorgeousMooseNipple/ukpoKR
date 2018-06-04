99bfe05d2fa5cdac4bd12c6b281faee8378740e6
GorgeousMooseNipple <orang768@gmail.com>
2018-06-04 03:45:49 +0300

    New unit-test. Close #2

--- DNAGraphTest/DNAPathsTests.cs
+++ DNAGraphTest/DNAPathsTests.cs
@@ -48,5 +48,57 @@ namespace DNAGraphTest
             Assert.AreEqual(verticles[1].GetNeighbours(), neighbours);
             Assert.AreEqual(verticles[2].GetNeighbours(), new List<String>());
         }
+
+        //Проверка корректности работы класса, разбивающего исходную молекулу на фрагменты
+        [Test]
+        public void FragmentGeneratorTest()
+        {
+            var fragments = FragmentsCreator.GetFragments("ADDCCFFDSSDSDSFDFDF", 10);
+            Assert.AreEqual(fragments.Length, 10);
+        }
+
+        // Проверка правильности нахождения путей в графе
+        [Test]
+        public void PathFindingTest()
+        {
+            String molecule = "АГЦЦГГУААЦЦ";
+            String[] fragmentsArray = { "АГЦЦ", "ЦГГУ", "ГГУАА", "УААЦЦ" };
+            DNAGraph graph = new DNAGraph(molecule, fragmentsArray);
+            List<List<String>> paths = graph.GetPaths();
+
+            List<String> path1 = new List<String>() { "АГЦЦ", "ЦГГУ", "ГГУАА", "УААЦЦ" };
+            List<String> path2 = new List<String>() { "АГЦЦ", "ЦГГУ", "УААЦЦ" };
+            List<String> path3 = new List<String>() { "АГЦЦ", "ГГУАА", "УААЦЦ" };
+
+            Assert.IsTrue(path1.SequenceEqual(paths[0]));
+            Assert.IsTrue(path2.SequenceEqual(paths[1]));
+            Assert.IsTrue(path3.SequenceEqual(paths[2]));
+        }
+
+        // Проверка правильности построения графа
+        [Test]
+        public void GraphBuildingTest()
+        {
+            String molecule = "АГЦЦГГУАП";
+            String[] fragmentsArray = { "АГЦЦ", "ЦГГУ", "ЦГГУ", "ГГУЦ", "ГУА" };
+            DNAGraph graph = new DNAGraph(molecule, fragmentsArray);
+            var verticlesList = graph.VerticleList;
+            var verticlesNeighboursFragments = new List<List<String>>() {
+                new List<String>() { "ЦГГУ" },
+                new List<String>() { "ГУА" },
+                new List<String>(),
+                new List<String>()
+               };
+
+            for (var i = 0; i < verticlesList.Count; ++i)
+            {
+                var neighboursFragments = new List<String>();
+                foreach (var neighbour in verticlesList[i].GetNeighbours())
+                {
+                    neighboursFragments.Add(neighbour.Fragment);
+                }
+                Assert.IsTrue(verticlesNeighboursFragments[i].SequenceEqual(neighboursFragments));
+            }
+        }
     }
 }
